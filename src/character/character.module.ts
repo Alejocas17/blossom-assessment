94b57f8d5ce90ebbeeaed251e38884500e0dcf14
Alejandro Castillo <castillogil.10@nlsa.teleperformance.com>
2025-07-10 21:20:00 -0500

    updated population function, now it populates and updates every 12 hours if there is changes available

--- src/character/character.module.ts
+++ src/character/character.module.ts
@@ -3,15 +3,16 @@ import { CharacterService } from './character.service';
 import { CharacterResolver } from './character.resolver';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Character } from './character.model';
+import { CharacterCronService } from './character-cron.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Character])],
-  providers: [CharacterService, CharacterResolver],
+  providers: [CharacterService, CharacterResolver, CharacterCronService],
 })
 export class CharacterModule {
   constructor(private readonly characterService: CharacterService) {}
 
   async onModuleInit() {
-    await this.characterService.seedInitialCharacters();
+    await this.characterService.syncCharactersFromAPI();
   }
 }
